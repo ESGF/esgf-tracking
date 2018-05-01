d0600f328c091fcd4bb40867e1ac1f929227293a
Sasha Ames <amysash2006@gmail.com>
2018-05-01 12:26:04 -0700

    indent

--- src/db_access.py
+++ src/db_access.py
@@ -14,29 +14,29 @@ class ESG_DB_Access:
     def  __init__():
     
 
-    self.engine = None
-    self.has_db = False
+        self.engine = None
+        self.has_db = False
 
-    if not os.path.exists(PASS_FN):
-        return 
-    
-    f = open(PASS_FN)
+        if not os.path.exists(PASS_FN):
+            return 
+        
+        f = open(PASS_FN)
 
-    passwd = f.read().strip()
-    
-#    properties_obj = get_prop_st()
+        passwd = f.read().strip()
+        
+    #    properties_obj = get_prop_st()
 
-    # Defaults based on conventional node installation
+        # Defaults based on conventional node installation
 
-    db_user = 'dbsuper'  # properties_obj.get('db.user', 'dbsuper')
-    db_host = 'localhost' # properties_obj.get('db.host', 'localhost')
-    db_port = '5432' # properties_obj.get('db.port', '5432')
-    db_database = 'esgcet' # properties_obj.get('db.database', 'esgcet')
-    db_str = ( 'postgresql://' + db_user +  ':' + passwd + '@' + db_host  +  ':' + db_port+ '/' + db_database)
+        db_user = 'dbsuper'  # properties_obj.get('db.user', 'dbsuper')
+        db_host = 'localhost' # properties_obj.get('db.host', 'localhost')
+        db_port = '5432' # properties_obj.get('db.port', '5432')
+        db_database = 'esgcet' # properties_obj.get('db.database', 'esgcet')
+        db_str = ( 'postgresql://' + db_user +  ':' + passwd + '@' + db_host  +  ':' + db_port+ '/' + db_database)
 
-    
-    self.engine = create_engine(db_str)
-    self.has_db = True
+        
+        self.engine = create_engine(db_str)
+        self.has_db = True
     
 
     def get_table():
