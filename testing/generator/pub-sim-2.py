82de9d875eacdf7f1e898815c0ac87dba5eb16c7
Sasha Ames <amysash2006@gmail.com>
2018-07-06 09:29:19 -0700

    fix some thangs

--- testing/generator/pub-sim-2.py
+++ testing/generator/pub-sim-2.py
@@ -54,7 +54,7 @@ def update_dset(path):
 
 	parts = path.split('/')
 
-	version_str = path[-1].rstrip()
+	version_str = parts[-1].rstrip()
 
 	version_year = int(version_str[1:5])
 	version_month = int(version_str[5:7])
@@ -68,7 +68,7 @@ def update_dset(path):
 	prepath = '/'.join(path[0:-1])
 
 
-	files = listdir(path)
+	files = os.listdir(path)
 
 	exec_cmd("mkdir " + prepath + 'files/d' + newverstr)
 	exec_cmd("cd " + prepath + '; ln -s files/d' + newverstr + ' v' + newverstr)
@@ -83,7 +83,7 @@ def update_dset(path):
 
 
 	publish_path(destpath)
-
+	return destpath
 
 def publish_path(path):
 
@@ -111,19 +111,23 @@ def main():
 
 	for i, idx in enumerate(dsarr): 
 
+		if i == TOT_PERIODS:
+			break
+
 		starttime = time()
 
 		if i < BASE:
 			dset = dset_list[idx]
 			print i, dset, "D-New-Dataset"
+			publish_path(dset)
 		else:
 			if i % PERIOD <  (PERIOD -2):
 
-				dset = dset_list[idx][0]
-				version = "v" + dset_list[idx][1]
+				dset = dset_list[idx]
+#				version = "v" + dset_list[idx][1]
 
 	#			append_to_list(outlst, dset, version, ts)
-				pub_list.append([dset,version])
+				pub_list.append([dset, "PUB"])
 
 				print i, dset, "D-New-Dataset"
 				publish_path(dset)				
@@ -133,7 +137,7 @@ def main():
 			elif i % PERIOD == (PERIOD - 2):
 				# new version should be from old datasets - try this - could be retracted
 
-				listlenbase = (len(outlst) - 2)
+				listlenbase = (len(pub_list) - 2)
 
 				dset_rec = None
 				choice = -1
@@ -148,35 +152,31 @@ def main():
 							print "find quick new version"
 						choice = ((TOT_PERIODS - 3) * PERIOD)
 						if DEBUG:
-							print "cat one", choice, len(outlst) 
-							print outlst[choice]
+							print "cat one", choice, len(pub_list) 
+							print pub_list[choice]
 					elif p_count == (TOT_PERIODS - 3):
 						if DEBUG:
 							print "find previous retraction"
 						choice =quick_retract
 						if DEBUG:
-							print "cat two", choice, len(outlst) 
-							print outlst[choice]
-						dset_rec = outlst[choice]
+							print "cat two", choice, len(pub_list) 
+							print pub_list[choice]
+						dset_rec = pub_list[choice]
 						break
 					else:
 						choice = int(rnd() * listlenbase)
-					dset_rec = outlst[choice]
+						dset_rec = pub_list[choice]
+						if dset_rec[1] == "PUB":
+							break
 
-					if dset_rec["latest"]:
-						break
 
 	#			print "len:", listlenbase, "choice", choice
 
-				dset = dset_rec["master_id"]
-				oldversion = dset_rec["version"]
-				numversion = int(oldversion[1:])
-				version = "v" + str(numversion + 1)
-				outlst[choice]["latest"] = False
+	
 
-				append_to_list(outlst, dset, version, ts)			
-				print i, choice, dset, "E-New-version", oldversion, version
-				update_dset(dset)
+				print i, choice, dset, "E-New-version"
+				newver  = update_dset(dset_rec[0])
+				pub_list[choice] = [newver, "PUB" ] 
 
 			else:
 				assert(i % PERIOD == PERIOD -1)
@@ -196,22 +196,26 @@ def main():
 						ret_str = "A-New-Retraction"
 						quick_retract= choice
 					
-					dset_rec = outlst[choice]
-					if (not dset_rec["retracted"]) and dset_rec["latest"]:
-						break
+				dset_rec = pub_list[choice]
 
-				dset = dset_rec["master_id"]
-				outlst[choice]["retracted"] = True
-				outlst[choice]["latest"] = False
+				# dset = dset_rec["master_id"]
+				# outlst[choice]["retracted"] = True
+				# outlst[choice]["latest"] = False
 
-				outlst[choice]["_timestamp"] = ts
-				print i, choice, dset, ret_str
+				# outlst[choice]["_timestamp"] = ts
+				print i, choice, dset_rec, ret_str
+				dset = dset_rec[0]
+				retract_dset(path_to_dset(dset))
+				pub_list[choice] = [dset, "RETR"]
+
+
 
 		endtime = time()
 
 		eltime = endtime - starttime
 
 		sleep (PUB_INTERVAL - eltime )
+
 
 	assert(p_count == TOT_PERIODS)
 
