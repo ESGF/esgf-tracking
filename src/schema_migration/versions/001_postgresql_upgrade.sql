c8c59d17fea13ab4b363da72e5f6a62d9a403eb9
root <root@esg-idx3.demonet.local>
2019-03-29 12:49:44 -0700

    update schema

--- src/schema_migration/versions/001_postgresql_upgrade.sql
+++ src/schema_migration/versions/001_postgresql_upgrade.sql
@@ -1,5 +1,5 @@
 create schema esgf_subscription ;
-create table esgf_subscription.subscribers (id integer primary key, email varchar(256), keyname varchar(64), valuename varchar(256)); 
+create table esgf_subscription.subscribers (id serial primary key, email varchar(256), keyname varchar(64), valuename varchar(256)); 
 create table esgf_subscription.keys (keyname varchar(64) primary key, project varchar(64));
 insert into esgf_subscription.keys values ('experiment', 'default');
 insert into esgf_subscription.keys values ('variable', 'default');
